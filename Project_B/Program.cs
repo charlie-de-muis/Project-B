7b69d237815a404d6ddd662cef1412e91fe8d4a6
1021342 <1021342@hr.nl>
2024-03-20 10:36:18 +0100

    Password encoding

--- Project_B/Program.cs
+++ Project_B/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine(PasswordEncoding.EncodeString("Hallo"));
+        Console.WriteLine(PasswordEncoding.DecodeString("ZsDDG"));
+    }
+
+    // assert.isequal unit test om te checken of de woorden goed encoded en decoded worden.
+    // assert.isequal unit test om te checken of hij errors goed kan handelen.
+}
