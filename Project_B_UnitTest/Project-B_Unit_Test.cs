fcfb8686dd57e5d7744a72c57737a9c0615b271e
Benny <145974469+Shookichis@users.noreply.github.com>
2024-06-06 15:02:36 +0200

    Update Project-B_Unit_Test.cs

--- Project_B_UnitTest/Project-B_Unit_Test.cs
+++ Project_B_UnitTest/Project-B_Unit_Test.cs
@@ -14,18 +14,18 @@ public class UnitTest1
     }
 // Hij kan het JSON bestand niet vinden. De code zelf zou moeten werken, we moeten alleen even kijken hoe we 
 // zorgen dat hij het bestand kan vinden.
-    // [TestMethod]
-    // public void Test_Add_Items()
-    // {
-    //     MenuItem Pizza = new MenuItem("Pizza pepperoni", new List<string> (){"dough", "sauce", "cheese", "pepperoni"}, 4.50, new List<string>(){"x"});
-    //     JSON.WriteJSON(new List<MenuItem>(){Pizza});
+    [TestMethod]
+    public void Test_Add_Items()
+    {
+        MenuItem Pizza = new MenuItem("Pizza pepperoni", new List<string> (){"dough", "sauce", "cheese", "pepperoni"}, 4.50, new List<string>(){"x"});
+        JSON.WriteJSON(new List<MenuItem>(){Pizza});
 
-    //     CollectionAssert.Contains(JSON.ReadJSON(), Pizza);
-    // }
+        CollectionAssert.Contains(JSON.ReadJSON(), Pizza);
+    }
 
     // [TestMethod]
     // public void Test_Saved_ReservationCode
     // {
-    //      hoe moet je het testen als er een input is?
+    //      //hoe moet je het testen als er een input is?
     // }
 }
