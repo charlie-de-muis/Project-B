cac398dc14c918c5bf299da877434a6c55f86e70
Benny <145974469+Shookichis@users.noreply.github.com>
2024-06-11 15:08:26 +0200

    Update Project-B_Unit_Test.cs

--- Project_B_UnitTest/Project-B_Unit_Test.cs
+++ Project_B_UnitTest/Project-B_Unit_Test.cs
@@ -2,7 +2,7 @@ namespace Project_B_UnitTest;
 
 [TestClass]
 public class UnitTest1
-{
+{   // Tiffany
     [TestMethod]
     public void EncodingTest()
     {
@@ -12,7 +12,7 @@ public class UnitTest1
         Assert.AreEqual(PasswordEncoding.DecodeString("ZsDDG"), "Hallo");
         Assert.AreEqual(PasswordEncoding.DecodeString("TQw"), "Bye");
     }
-
+    // Tiffany
     [TestMethod]
     public void Test_Add_Items()
     {
@@ -24,7 +24,7 @@ public class UnitTest1
         Assert.AreEqual(Pizza.Name, latest.Name);
         Assert.AreEqual(Pizza.Price, latest.Price);
     }
-
+    // Tiffany
     [TestMethod]
     public void Test_Saved_ReservationCode()
     {
@@ -33,5 +33,28 @@ public class UnitTest1
 
         Reservation latest = CSV.ReadFromCSVReservations("Reservation.csv", true).FirstOrDefault(res => res.Date == "12-12-2030");
         Assert.AreEqual(latest.ReservationCode, res.ReservationCode);
+    }
+
+    //Bente
+    [TestMethod]
+    public void Test_Sort_Price()
+    {
+        // Arrange
+        MenuItem Pizza = new MenuItem(1000, "Pizza pepperoni", new List<string> {"dough", "sauce", "cheese", "pepperoni"}, 4.50, new List<string> {"x"});
+        MenuItem Burger = new MenuItem(1001, "Cheeseburger", new List<string> {"bun", "cheese", "beef"}, 3.00, new List<string> {"y"});
+        MenuItem Salad = new MenuItem(1002, "Caesar Salad", new List<string> {"lettuce", "dressing", "croutons"}, 5.00, new List<string> {"z"});
+        List<MenuItem> menu = new List<MenuItem> { Pizza, Burger, Salad };
+
+        // Mock JSON.ReadJSON method by directly setting the menu list
+        JSON.WriteJSON(menu, "current", true);
+
+        // Act
+        MenuManager.DisplaySortedMenu();
+
+        // Assert
+        var sortedMenu = JSON.ReadJSON("current", true).OrderBy(item => item.Price).ToList();
+        Assert.AreEqual(Burger.ID, sortedMenu[0].ID); // Burger should be first (cheapest)
+        Assert.AreEqual(Pizza.ID, sortedMenu[1].ID);  // Pizza should be second
+        Assert.AreEqual(Salad.ID, sortedMenu[2].ID);  // Salad should be third (most expensive)
     }
 }
