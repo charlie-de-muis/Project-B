154c5e269bfca5d1186c8402ef4f93131258dc0f
orestis <1071209@hr.nl>
2024-06-14 10:42:51 +0200

    Update Delete_Item.cs

--- Project_B/Logic/Delete_Item.cs
+++ Project_B/Logic/Delete_Item.cs
@@ -1,19 +1,28 @@
+// Made by Melvern
+
 public class Delete_Item
 {
     public static void Delete_Items()
     {
-        List<MenuItem> menuItems = JSON.ReadJSON("Menu_current");
+        string prompt = "Which menu are you editing?";
+        string[] options = { "Current Menu", "Future Menu" };
+        string menuType = ConsoleGUI.OptionGUI(prompt, options) == 0 ? "Menu_current" : "Menu_future";
+
+        // load the menu
+        List<MenuItem> menuItems = JSON.ReadJSON(menuType, false);
         
         while (true)
         {
-            MenuManager.DisplayMenu();
+            MenuManager.DisplayMenu(menuType);
 
+            // ask which item needs to be deleted
             Console.WriteLine("\nWhich item do you want to delete? Enter an index or type cancel.");
             string choice = Console.ReadLine();
             Program.ConsoleClear();
 
             if (choice.ToLower() == "cancel") {return;}
             
+            // check if the answer is valid
             if (!int.TryParse(choice, out int index))
             {
                 Console.WriteLine("Invalid entry. Please enter a correct item index.\nPress enter to continue...");
@@ -21,18 +30,20 @@ public class Delete_Item
                 continue;
             }
             
-            menuItems = DeleteChosenItem(index, menuItems);
+            menuItems = DeleteChosenItem(index, menuItems, menuType);
         }
     }
 
-    public static List<MenuItem> DeleteChosenItem(int index, List<MenuItem> menuItems)
+    public static List<MenuItem> DeleteChosenItem(int index, List<MenuItem> menuItems, string menuType)
     {
         try
         {
+            // remove the item from the menu
             int removed = menuItems.RemoveAll(menuItem => menuItem.ID == index);
             if (removed > 0)
             {
-                JSON.DeletedItemsWriteJSON(menuItems, "Menu_current");
+                // rewrite the menu
+                JSON.DeletedItemsWriteJSON(menuItems, menuType);
             }
             else
             {
